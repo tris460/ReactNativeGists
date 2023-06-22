f18e8e9d63a045fcf8e4cc3e1c6c1a8ceadcfdb7
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-06-22 12:14:26 -0700

    Add hello world in a different file

--- App.tsx
+++ App.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import HelloWorld from './src/screens/HelloWorld'
 
 const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center' }}>
-      <Text style={{
-        fontSize: 30,
-        textAlign: 'center'
-      }}>
-        Hola Mundo
-        </Text>
-    </View>
+    <HelloWorld />
   )
 }
 
---
+++ src/screens/HelloWorld.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorld = () => {
+  return (
+    <View style={{
+        flex: 1,
+        justifyContent: 'center' }}>
+        <Text style={{
+          fontSize: 30,
+          textAlign: 'center'
+        }}>
+          Hello World
+          </Text>
+      </View>
+    )
+}
+
+export default HelloWorld;

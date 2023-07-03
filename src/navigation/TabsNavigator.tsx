7899a720c8173381cc9325badc8b60c081ded211
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-07-03 09:52:10 -0700

    Add bottom tab for android

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -7,13 +7,15 @@ import Dimentions from '../screens/Dimentions';
 import HelloWorld from '../screens/HelloWorld';
 import MenuStackNavigator from '../screens/MenuStackNavigator';
 import Position from '../screens/Position';
-import { Text } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Platform, Text } from 'react-native';
 
-const Tab = createBottomTabNavigator();
-
-export const TabsNavigator = () => {
+const BottomTabIOS = createBottomTabNavigator();
+const BottomTabAndroid = createMaterialBottomTabNavigator();
+    
+const TabsNavigatorIOS = () => {
   return (
-    <Tab.Navigator
+    <BottomTabIOS.Navigator
         sceneContainerStyle={{ backgroundColor: '#fff' }}
         screenOptions={({route}) => ({
             tabBarActiveTintColor: '#9132a8',
@@ -48,13 +50,71 @@ export const TabsNavigator = () => {
             }
         })}
         >
-      <Tab.Screen name="MenuStackNavigator" options={{ title: 'Menu' }} component={MenuStackNavigator} />
-      <Tab.Screen name="BoxObjectModel" component={BoxObjectModel} />
-      <Tab.Screen name="Calculator" component={Calculator} />
-      <Tab.Screen name="Counter" component={Counter} />
-      <Tab.Screen name="Dimentions" component={Dimentions} />
-      <Tab.Screen name="HelloWorld" component={HelloWorld} />
-      <Tab.Screen name="Position" component={Position} />
-    </Tab.Navigator>
+      <BottomTabIOS.Screen name="MenuStackNavigator" options={{ title: 'Menu' }} component={MenuStackNavigator} />
+      <BottomTabIOS.Screen name="BoxObjectModel" component={BoxObjectModel} />
+      <BottomTabIOS.Screen name="Calculator" component={Calculator} />
+      <BottomTabIOS.Screen name="Counter" component={Counter} />
+      <BottomTabIOS.Screen name="Dimentions" component={Dimentions} />
+      <BottomTabIOS.Screen name="HelloWorld" component={HelloWorld} />
+      <BottomTabIOS.Screen name="Position" component={Position} />
+    </BottomTabIOS.Navigator>
   );
+}
+
+const TabsNavigatorAndroid = () => {
+  return (
+    <BottomTabAndroid.Navigator
+        barStyle = {{
+            backgroundColor: '#a86d93',
+        }}
+        activeColor='#1600de'
+        inactiveColor='#fff'
+        screenOptions={({route}) => ({
+            tabBarIcon: ({color, focused}) => {
+                let iconName: string = '';
+                switch(route.name) {
+                    case 'MenuStackNavigator':
+                        iconName='MSN'
+                        break;
+                    case 'BoxObjectModel':
+                        iconName='BOM'
+                        break;
+                    case 'Calculator':
+                        iconName='Ca'
+                        break;
+                    case 'Counter':
+                        iconName='Co'
+                        break;
+                    case 'Dimentions':
+                        iconName='Di'
+                        break;
+                    case 'HelloWorld':
+                        iconName='HW'
+                        break;
+                    case 'Position':
+                        iconName='Po'
+                        break;
+                }
+                return <Text style={{color}}>{iconName}</Text>
+            }
+        })}
+    >
+        <BottomTabAndroid.Screen name="MenuStackNavigator" options={{ title: 'Menu' }} component={MenuStackNavigator} />
+        <BottomTabAndroid.Screen name="BoxObjectModel" component={BoxObjectModel} />
+        <BottomTabAndroid.Screen name="Calculator" component={Calculator} />
+        <BottomTabAndroid.Screen name="Counter" component={Counter} />
+        <BottomTabAndroid.Screen name="Dimentions" component={Dimentions} />
+        <BottomTabAndroid.Screen name="HelloWorld" component={HelloWorld} />
+        <BottomTabAndroid.Screen name="Position" component={Position} />
+    </BottomTabAndroid.Navigator>
+  );
+    
+}
+
+export const TabsNavigator = () => {
+    return (
+        Platform.OS === 'ios' ?
+            <TabsNavigatorIOS /> : <TabsNavigatorAndroid />
+        
+    )
 }

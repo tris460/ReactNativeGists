806456ac952b89d9e8122172e7e72e31ea149613
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-07-05 10:54:26 -0700

    Add result's interface

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 import movieDb from '../api/movieDb'
+import { MovieDBNowPlaying } from '../interfaces/movieInterface'
 
 const Home = () => {
     useEffect(() => {
-        movieDb.get('/now_playing')
+        movieDb.get<MovieDBNowPlaying>('/now_playing')
         .then(res => console.log(res.data))
     })
 

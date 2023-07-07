e6c348815e65052339f83cad84541f5055b57fb6
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-07-07 14:05:06 -0700

    Add bounce login

--- src/screens/BounceAnimation.tsx
+++ src/screens/BounceAnimation.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useRef } from 'react'
+import { Animated, PanResponder, Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 
 const BounceAnimation = () => {
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event(
+        [
+            null,
+            {
+                dx: pan.x,
+                dy: pan.y,
+            },
+        ], {
+            useNativeDriver: false
+        }),
+        onPanResponderRelease: () => {
+        Animated.spring(
+            pan,
+            {toValue: {x: 0, y: 0}, useNativeDriver: false},
+        ).start();
+        },
+    });
+
     return (
         <View style={{
             ...styles.container, 
             justifyContent: 'center', 
             alignItems:'center'}}>
-            <View style={styles.box} />
+            <Text style={styles.title}>
+                Bounce Animation
+            </Text>
+            <Animated.View 
+                {...panResponder.panHandlers}
+                style={[styles.box, pan.getLayout()]} />
         </View>
     )
 }

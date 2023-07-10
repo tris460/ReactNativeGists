9a34a99d1495d7ef9bb5bd42dd6bbcf87d75c0fd
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-07-10 13:31:15 -0700

    Add types to pokemons

--- src/hooks/usePokemonPaginator.tsx
+++ src/hooks/usePokemonPaginator.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import pokemonApi from '../api/pokemonApi';
+import { PokemonResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 const usePokemonPaginator = () => {
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
 
     useEffect(() => {
-        loadPokemons();
+        loadPokemon();
     }, []);
     
-    const loadPokemons = async() => {
-        const res = await pokemonApi.get(nextPageUrl.current);
-        console.log(res.data);
+    const loadPokemon = async() => {
+        const res = await pokemonApi.get<PokemonResponse>(nextPageUrl.current);
+        nextPageUrl.current = res.data.next;
+        mapPokemonList(res.data.results);
+    }
+
+    const mapPokemonList = (pokemonList: Result[]) => {
+        pokemonList.forEach(poke => console.log(poke.name))
     }
 
     return (

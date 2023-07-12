cb7129f8fc9f1e0d8055b51735d48b122c3f0abc
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2023-07-12 09:23:38 -0700

    Create context

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/navigation/StackNavigation';
+import { PermissionsProvider } from './src/context/PermissionContext';
+
+const AppState = ({children}: any) => {
+  return (
+    <PermissionsProvider>
+      {children}
+    </PermissionsProvider>
+  )
+}
 
 const App = () => {
   return (
-      <NavigationContainer>
+    <NavigationContainer>
+      <AppState>
         <StackNavigation />
-      </NavigationContainer>
+      </AppState>
+    </NavigationContainer>
   )
 }
 
---
+++ src/context/PermissionContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from 'react';
+import { PermissionStatus } from 'react-native-permissions';
+
+export interface PermissionsState {
+    locationStatus: PermissionStatus;
+}
+
+export const permissionInitState: PermissionsState = {
+    locationStatus: 'unavailable',
+}
+
+type PermissionsContextProps = {
+    permissions: PermissionsState,
+    askLocationPermission: () => void;
+    checkLocationPermission: () => void;
+}
+
+export const PermissionsContext = createContext({} as PermissionsContextProps);
+
+export const PermissionsProvider = ({children}: any) => {
+    const [permissions, setPermissions] = useState(permissionInitState);
+
+    const askLocationPermission = () => {};
+    const checkLocationPermission = () => {};
+
+    return(
+        <PermissionsContext.Provider value={{
+            permissions,
+            askLocationPermission,
+            checkLocationPermission,
+        }}>
+            {children}
+        </PermissionsContext.Provider>
+    )
+}
